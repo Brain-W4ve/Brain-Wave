a29f9a7485a874680d0f3fe84d12f26ae9ba68c5
Brandon Gutierrez <casillasbranguti07@gmail.com>
2025-02-12 20:03:11 -0600

    Delete backend directory

--- backend/src/models/file_data.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, LargeBinary, DateTime
-from . import Base
-
-class File_Data(Base):
-    __tablename__ = "file_data"
-    id = Column(Integer, primary_key=True)
-    # timestamp = Column(DateTime, nullable=False)
-    filename = Column(String(255), nullable=False)
-    data = Column(LargeBinary, nullable=False)
