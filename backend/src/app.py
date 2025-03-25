893a8ccc71e865ff5440602325c351b3b34e9135
Carlos Martinez <alberto-cmg@outlook.es>
2025-03-24 22:51:24 -0600

    working Dockerfile for flask, and minio, missing grpc and frontend

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,7 @@
 from src import create_app
 
-if __name__ == "__main__":
-    app = create_app()
-    # app.run(host="0.0.0.0.0", debug=True)
-    # app.run(host="0.0.0.0", port=5000, debug=True)
-    app.run(debug=True)
+app = create_app()
+
+
+if __name__ == "__main__": 
+    app.run(debug=True, host="0.0.0.0", port=5000)
