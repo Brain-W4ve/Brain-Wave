9e0fb5631569c9cf27101246031d4ed35629d6f7
Brandon Gutierrez <casillasbranguti07@gmail.com>
2025-03-25 19:36:57 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom"; 
 import LoginPage from "./components/LoginPage.jsx";
 import AboutUs from "./components/AboutUs.jsx";
-import Menu from "./components/Menu.jsx";
+import Menu from "./components/menu.jsx";
+import Dashboard from "./components/dashboard.jsx"
+import Register from "./components/RegisterForm.jsx"
 
 export default function App() {
   return (
     <Routes> {}
       <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/about" element={<AboutUs />} />
       <Route path="/menu" element={<Menu />} />   
     </Routes>
